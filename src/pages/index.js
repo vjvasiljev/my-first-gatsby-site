643c26e287b8df71a52365351b8891a21571b1fb
Viaceslav <v.j.vasiljev@gmail.com>
2022-02-20 08:55:19 +0200

    Add optimased image loading via gatsby static image plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const indexPage = () => {
   return (
     <Layout pageTitle={"Home page"}>
       <p>I'm making this by following the gatsby tutorial</p>
+      <StaticImage
+        alt="An image of high office building in Klaipeda"
+        src="..\images\uab-statega-apskon-1.jpg"
+      ></StaticImage>
     </Layout>
   );
 };
